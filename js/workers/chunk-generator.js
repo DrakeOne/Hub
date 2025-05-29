47d3f042bd96aa2b138aee1c2ec613ca972872fa
DrakeOne <124852325+DrakeOne@users.noreply.github.com>
2025-05-29 17:37:58 -0400

    Update chunk-generator.js

--- js/workers/chunk-generator.js
+++ js/workers/chunk-generator.js
@@ -2,7 +2,7 @@
 // Esto elimina el lag al generar nuevos chunks
 
 // Importar SimplexNoise
-importScripts('https://drakeone.github.io/Hub/js/libs/simplex-noise.min.js');
+importScripts('https://cdn.jsdelivr.net/npm/simplex-noise@2.4.0/simplex-noise.min.js');
 
 // Constantes necesarias (copiadas de Constants.js)
 const CHUNK_SIZE = 16;
